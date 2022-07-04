060c86f135f974eea77b289a3da36a746a19bdd8
Steven Hall <Hallzy.18@gmail.com>
2022-07-04 00:54:23 -0700

    instagram_reduce_focus_of_videos now also makes pinned posts invisible

--- instagram_reduce_focus_of_videos.js
+++ instagram_reduce_focus_of_videos.js
@@ -3,8 +3,13 @@ javascript:(function() {
         document.querySelectorAll('article > div:first-child > div > div > div').forEach(el => {
             const svgClip = el.querySelector('svg[aria-label="clip"i]');
             const svgVid  = el.querySelector('svg[aria-label="video"i]');
+            const svgPin  = el.querySelector('svg[aria-label="pinned post icon"i]');
             if (svgClip || svgVid) {
                 el.style.opacity = '0.1';
+            }
+
+            if (svgPin) {
+                el.style.opacity = '0';
             }
         });
     };
